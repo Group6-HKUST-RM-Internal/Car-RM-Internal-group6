481ef72f023e1323e218da95808b98e4806b8a8a
cweiai <cweiai@connect.ust.hk>
2024-10-23 21:34:00 +0800

    add FSi6x.hpp

--- Core/Inc/FSi6X.hpp
+++ Core/Inc/FSi6X.hpp
@@ -1,25 +1,14 @@
 #include "main.h"
 
-#include "stdint.h"
+#include "string.h"
 
-#pragma once
+#proama once
 
-namespace FSi6X
+namespace FSi6x
 {
     typedef struct RcData;
-
-    // Get the decoded remote controller data
+    
     const RcData *getRcData();
 
-    // Reset the data
-    void resetData();
-
-    // Receive callback
-    void rxCallback(UART_HandleTypeDef *huart, uint16_t size);
-
-    // Error callback
-    void erCallback(UART_HandleTypeDef *huart);
-
-    // Initialize the FSi6X module
     void init();
 }
