6568ed44cdd953f19b063d54fcb236d181c40993
Aerospace Rabbit <yzhangqm@connect.ust.hk>
2024-10-24 22:27:18 +0800

    Final version

--- Core/Src/UserTask.cpp
+++ Core/Src/UserTask.cpp
@@ -21,32 +21,32 @@ StaticTask_t xMainTaskTCB;
 // Define the motor
 M3508 m3508[4];
 
+static int16_t current[4]; // Array of current for each motor. ID=1 motor corresponds to current[0], and so on
 
-static int16_t current[4]; //Array of current for each motor. ID=1 motor corresponds to current[0], and so on
-
-static PID rpm_PID(500,50,0); // PID controller for RPM
+static PID rpm_PID(500, 50, 0); // PID controller for RPM
 
 static float targetRPM[4]; // target rpm
 
-
 // an example task
-void mainTask(void *pvPara) {
+void mainTask(void *pvPara)
+{
   // Initialize the motor
   initMotor(m3508);
-  while (1) {
+  while (1)
+  {
+
     vTaskDelay(1);
     // Delay the whole program for 1ms after executing a round of loop
     // This is important because we do not want our program to refresh too fast.
   }
-  
-    
 }
 
 /**
  * @brief Intialize all the drivers and add task to the scheduler
  * @todo  Add your own task in this file
  */
-void startUserTasks() {
+void startUserTasks()
+{
   xTaskCreateStatic(mainTask, "testTask", configMINIMAL_STACK_SIZE, NULL, 0,
                     uxMainTaskStack, &xMainTaskTCB);
   /**
